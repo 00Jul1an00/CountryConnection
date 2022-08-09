c83531cfdac50f26892afa16d265f3c56fcba880
00Jul1an00 <mrjulik.beletsky@yandex.ru>
2022-08-09 20:07:57 +0300

    3

--- CountryConectionGame/Assets/Scripts/Raycaster.cs
+++ CountryConectionGame/Assets/Scripts/Raycaster.cs
@@ -1,30 +1,64 @@
 using UnityEngine;
 
+/// <summary>
+/// Œ“–≈‘¿ “Œ–»“‹
+/// </summary>
+/// 
 public class Raycaster : MonoBehaviour
 {
     private Vector2 _originVector;
     private Vector2 _endVector;
+    private bool _isConnected;
+    private Town _startTown;
+    private Town _endTown;
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        { 
             if(Physics.Raycast(ray, out var hit))
             {
                 if(hit.collider.TryGetComponent(out Town town))
                 {
                     _originVector = hit.transform.position;
-                    print(_originVector.x + " " + _originVector.y);
-                    if(Input.GetMouseButtonUp(0))
-                    {
-                        _endVector = hit.transform.position;
-                        print(_endVector.x + " " + _endVector.y);
-                        float finalVector = Vector2.Distance(_originVector, _endVector);
-                        print(finalVector);
-                    }
+                    _startTown = town;
                 }
             }
         }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(Physics.Raycast(ray, out var hit))
+            {
+                if (hit.collider.TryGetComponent(out Town town))
+                {
+                    _endTown = town;
+                    _endVector = hit.transform.position;
+                    Ray checkCollisionRay = new Ray(_originVector, _endVector - _originVector);
+                    float distance = Vector2.Distance(_endVector, _originVector);
+
+                    if (Physics.Raycast(checkCollisionRay, out var hitInfo, distance))
+                    {
+                        if (hitInfo.collider.TryGetComponent(out Town t))
+                        {
+                            _isConnected = (t == _startTown || t == _endTown);
+                            Debug.Log(_isConnected);
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+
+        _endTown = null;
+        _startTown = null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(_originVector, _endVector - _originVector);
     }
 }

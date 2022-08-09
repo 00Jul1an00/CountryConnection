5ce20837e2709473160bb9b374b02a26a2d48f1b
00Jul1an00 <mrjulik.beletsky@yandex.ru>
2022-08-09 22:50:51 +0300

    Update Town.cs

--- CountryConectionGame/Assets/Scripts/Town.cs
+++ CountryConectionGame/Assets/Scripts/Town.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Town : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public event UnityAction<Town> TownClicked;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private void OnMouseUpAsButton() => TownClicked?.Invoke(this);
 }

e5cd69a930d15ffdc1eb2b630d2058638a717790
Bonifa32 <Salmon3221@mail.ru>
2022-08-16 02:01:18 +0300

    бебровидный

--- CountryConectionGame/Assets/Scripts/Town.cs
+++ CountryConectionGame/Assets/Scripts/Town.cs
@@ -3,17 +3,24 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Town : MonoBehaviour
 {
     [SerializeField] private GameObject _upgradePanel;
-
-    private int _townGrade;
+    [SerializeField] private Button _upgradeButton;   
+    private int _townGrade = 1;
     private int _currentGrade;
-    private int _gradeCost;
+    private int _gradeCost = 100;
     private int _currentCost;
+    private float _localMoney;
 
-
+    public event UnityAction<float> OnMoneyChanged;
+    private void Start()
+    {
+        _localMoney = Money._money;
+        
+    }
     private void OnMouseUpAsButton()
     {        
         _upgradePanel.SetActive(true);
@@ -28,6 +35,21 @@ public class Town : MonoBehaviour
     private void OnMouseExit()
     {
         StartCoroutine(ClosePanel());
+    }
+
+    public void _upgradeClick()
+    {
+        if (_localMoney >= _gradeCost)
+        {
+            _localMoney -= _gradeCost;
+            OnMoneyChanged?.Invoke(_localMoney);
+            _gradeCost += (_gradeCost * _townGrade + 1);
+            _townGrade++;
+        }
+        else
+        {
+            Debug.Log("У вас недостаточно средств!");
+        }
     }
 }
     

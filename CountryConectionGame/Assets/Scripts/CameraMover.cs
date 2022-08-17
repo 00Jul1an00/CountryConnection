681c5a25beba227b2ea70c7fc963646f2f742105
00Jul1an00 <mrjulik.beletsky@yandex.ru>
2022-08-17 16:50:17 +0300

    cam fix

--- CountryConectionGame/Assets/Scripts/CameraMover.cs
+++ CountryConectionGame/Assets/Scripts/CameraMover.cs
@@ -5,34 +5,56 @@ using UnityEngine;
 public class CameraMover : MonoBehaviour
 {
     [SerializeField] private float _camereMovingSpeed;
+    [SerializeField] private float _zoomSpeed;
 
     private Vector3 _startPos;
     private float _targetPosX;
     private float _targetPosY;
+    private Camera _cam;
+
+    private void Start() => _cam = GetComponent<Camera>();
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        CameraZoomCheck();
+        ChangeCameraZoom(Input.mouseScrollDelta.y < 0, Input.mouseScrollDelta.y > 0, 1f);
+
+        if (Input.GetMouseButtonDown(1))
         {
-            _startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition).normalized;
+            _startPos = _cam.ScreenToWorldPoint(Input.mousePosition);
         } 
-        else if(Input.GetMouseButton(0))
+        else if(Input.GetMouseButton(1))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
             
             if(!Physics.Raycast(ray, Mathf.Infinity, 6) && !PathBuilder.StartingBuildPath)
             {
-                Vector3 pos = ray.origin.normalized - _startPos;
+                Vector3 pos = _cam.ScreenToWorldPoint(Input.mousePosition) - _startPos;
 
                 _targetPosX = transform.position.x - pos.x;
                 _targetPosY = transform.position.y - pos.y;
-
-                //transform.position = new Vector3(transform.position.x - pos.x, transform.position.y - pos.y, transform.position.z);
             }
         }
 
         transform.position = new Vector3(Mathf.Lerp(transform.position.x, _targetPosX, _camereMovingSpeed * Time.deltaTime),
                                          Mathf.Lerp(transform.position.y, _targetPosY, _camereMovingSpeed * Time.deltaTime),
                                          transform.position.z);
+    }
+
+    private void CameraZoomCheck()
+    {
+        if (_cam.orthographicSize <= 3)
+            _cam.orthographicSize = 3;
+        else if (_cam.orthographicSize >= 15)
+            _cam.orthographicSize = 15;
+    }
+
+    private void ChangeCameraZoom(bool FirstCondition, bool SecondCondition, float size)
+    {
+        if(FirstCondition)
+            _cam.orthographicSize += size * Time.deltaTime * _zoomSpeed;
+        else if(SecondCondition)
+            _cam.orthographicSize -= size * Time.deltaTime * _zoomSpeed;
+
     }
 }
--- CountryConectionGame/Assets/Scripts/Money.cs
+++ CountryConectionGame/Assets/Scripts/Money.cs
@@ -21,8 +21,8 @@ public class Money : MonoBehaviour
         _townArray = new Town[_townCount];
         for (int i = 0; i < _townCount; i++)
         {
-            _townArray[i] = _parentTown.
-            _townArray[i].MoneyChanged += OnMoneyChanged;
+            //_townArray[i] = _parentTown.
+            //_townArray[i].MoneyChanged += OnMoneyChanged;
         }
         _localPassiveIncome.MoneyChanged += OnMoneyChanged;
         OnMoneyChanged(PlayerMoney);
